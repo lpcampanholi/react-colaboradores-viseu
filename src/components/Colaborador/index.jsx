ba881ebddff4293e8dd39a947a5810a15db49648
Luís Campanholi <lpcampanholi@gmail.com>
2024-02-18 10:33:52 -0300

    muda de setor

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -3,7 +3,7 @@ import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 import { FaLinkedin, FaInstagram } from "react-icons/fa";
 import { IoMdCloseCircle } from "react-icons/io";
 
-function Colaborador({ colaborador, cor, aoExcluir, favoritar, setores, mudaDeSetor }) {
+function Colaborador({ colaborador, setores, cor, aoExcluir, favoritar, mudaDeSetor }) {
 
   return (
     <div className="colaborador">
@@ -21,10 +21,11 @@ function Colaborador({ colaborador, cor, aoExcluir, favoritar, setores, mudaDeSe
         <p>{colaborador.cargo}</p>
 
         <div className='etiqueta' style={{ backgroundColor: cor }}>
-          <select className="banana" name={colaborador.setor} id={colaborador.setor} onChange={(e) => mudaDeSetor(colaborador.id, e.target.value)} >
-            <option className="heineken" value={colaborador.setor}>{colaborador.setor}</option>
-            {setores.filter(setor => setor.nome !== colaborador.setor)
-              .map(setor => <option className="heineken" key={setor.id} value={setor.nome}>{setor.nome}</option>)}
+          <select className="select" name={colaborador.setor} id={colaborador.setor} onChange={(e) => mudaDeSetor(colaborador.id, e.target.value)} >
+            <option className="option" value={colaborador.setor}>{colaborador.setor}</option>
+            {setores
+              .filter(setor => setor.nome !== colaborador.setor)
+              .map(setor => <option className="option" key={setor.id} value={setor.nome}>{setor.nome}</option>)}
           </select>
         </div>
 
